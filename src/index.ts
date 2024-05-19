5f75b138cb3724b0296185e604ac4123424736b5
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-05-19 04:11:43 +0100

    configured the linter and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
-import http from "http";
+import http from 'node:http';
 
-export const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end(JSON.stringify({ data: "It works!!" }));
+export const server = http.createServer((request, response) => {
+	response.writeHead(200, {'content-type': 'application/json'});
+	response.end(JSON.stringify({data: 'It works!'}));
 });
 
 server.listen(3000, () => {
-  console.log(`We are running on port: 3000`);
-
-  console.log("jdjdjssd");
+	console.log('We are running on port: 3000');
 });
