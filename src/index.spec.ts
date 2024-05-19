7644d82e2272be4007da3256f6bc94f3a0fcfaf5
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-05-19 11:14:57 +0100

    completed the project setup

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,17 +1,17 @@
 import supertest from 'supertest';
-import {server} from './index';
+import { server } from './index';
 
 describe('server', () => {
-	const request = supertest.agent(server);
+  const request = supertest.agent(server);
 
-	afterAll((done) => {
-		server.close(done);
-	});
+  afterAll((done) => {
+    server.close(done);
+  });
 
-	it('should get /', async () => {
-		const response = await request.get('/');
+  it('should get /', async () => {
+    const response = await request.get('/');
 
-		expect(response.status).toBe(200);
-		expect(response.body).toEqual({data: 'It works!'});
-	});
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ data: 'It works!' });
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import http from 'node:http';
+import { randomUUID } from 'node:crypto';
+import bunyan from 'bunyan';
 
-export const server = http.createServer((request, response) => {
-	response.writeHead(200, {'content-type': 'application/json'});
-	response.end(JSON.stringify({data: 'It works!'}));
+const logger = bunyan.createLogger({
+  name: 'APPLICATION_ROOT',
+  src: false,
+  level: 'trace',
+  serializers: bunyan.stdSerializers,
 });
 
-server.listen(3000, () => {
-	console.log('We are running on port: 3000');
+export const server = http.createServer((request, response) => {
+  response.writeHead(200, { 'content-type': 'application/json' });
+  response.end(JSON.stringify({ data: 'It works!' }));
+});
+
+server.listen(3001, () => {
+  const trace = logger.child({ req_id: randomUUID() });
+
+  trace.info('We are running on port: 3000');
+  trace.warn('We found the bug');
+
+  const someError = new Error('Ooops! Something went wrong :(');
+  trace.error(someError, 'Something went wrong');
 });
