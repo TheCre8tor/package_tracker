779c8b5b69577c3391274d0a5a138f4ef6194521
Alexander Nitiola <cre8tor.alexander@gmail.com>
2024-05-24 23:34:03 +0100

    setup drizzle database integration

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,33 +1,33 @@
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import {drizzle} from 'drizzle-orm/node-postgres';
+import {Pool} from 'pg';
 import Logger from 'bunyan';
-import { DefaultLogger, LogWriter, Logger as DrizzleLogger } from 'drizzle-orm';
+import {DefaultLogger, LogWriter, Logger as DrizzleLogger} from 'drizzle-orm';
 import Config from '../configs/configuration';
 import * as schema from './schemas/schema';
 
 export class ClassicLogger implements DefaultLogger {
-  writer: LogWriter;
+	writer: LogWriter;
 
-  constructor(
-    private readonly logger: Logger,
-    config?: {
-      writer: LogWriter;
-    },
-  ) {}
+	constructor(
+		private readonly logger: Logger,
+		config?: {
+			writer: LogWriter;
+		},
+	) {}
 
-  logQuery(query: string, parameters: unknown[]): void {
-    this.logger.info(`${query}`);
-  }
+	logQuery(query: string, parameters: unknown[]): void {
+		this.logger.info(`${query}`);
+	}
 }
 
 export const pool = new Pool({
-  host: Config.DATABASE_HOST,
-  user: Config.DATABASE_USERNAME,
-  password: Config.DATABASE_PASSWORD,
-  database: Config.DATABASE_NAME,
+	host: Config.DATABASE_HOST,
+	user: Config.DATABASE_USERNAME,
+	password: Config.DATABASE_PASSWORD,
+	database: Config.DATABASE_NAME,
 });
 
 export function connect(logger: DrizzleLogger) {
-  const database = drizzle(pool, { schema, logger });
-  return database;
+	const database = drizzle(pool, {schema, logger});
+	return database;
 }
